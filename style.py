b915d2923d686ea8d3d6b56ad33bf90f2a8db18c
tkf-data17 <tayif01@gmail.com>
2025-08-13 09:26:31 +0000

    changement couleur

--- style.py
+++ style.py
@@ -87,7 +87,7 @@ body {{
     padding-left: 100%;
     animation: scroll-left 50s linear infinite;
     font-size: 16px;
-    color: darkblue;
+    color: yellow;
     font-weight: bold;
     line-height: 30px;
 }}
